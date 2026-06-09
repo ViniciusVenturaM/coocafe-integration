4a24a6bc043732bedcb15cc2d81a4aee502e81f4
ViniciusVenturaM <vini.ventura2@gmail.com>
2026-06-09 10:52:19 -0300

    comenta rota de teste

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,20 @@ use App\Http\Controllers\ApiController;
 use App\Http\Controllers\LoginController;
 
 
-Route::get('/teste-autenticacao-secreta', function () {
-    // 1. Busca o seu usuário no banco da Railway
-    $user = \App\Models\User::where('usuario', 'vinicius.ventura')->first();
+// Route::get('/teste-autenticacao-secreta', function () {
+//     // 1. Busca o seu usuário no banco da Railway
+//     $user = \App\Models\User::where('usuario', 'vinicius.ventura')->first();
     
-    if (!$user) {
-        return 'ERRO: Usuário vinicius.ventura não foi encontrado no banco da Railway. Certifique-se de que rodou o INSERT lá primeiro!';
-    }
+//     if (!$user) {
+//         return 'ERRO: Usuário vinicius.ventura não foi encontrado no banco da Railway. Certifique-se de que rodou o INSERT lá primeiro!';
+//     }
     
-    // 2. Atualiza a senha forçando a criptografia nativa do ambiente atual
-    $user->password = 'coocafe2026'; // O Cast 'hashed' do Model faz o trabalho sozinho
-    $user->save();
+//     // 2. Atualiza a senha forçando a criptografia nativa do ambiente atual
+//     $user->password = 'coocafe2026'; // O Cast 'hashed' do Model faz o trabalho sozinho
+//     $user->save();
     
-    return 'SUCESSO! A senha foi recriptografada e salva direto pelo Laravel na Railway. Agora remova esse código e tente logar na tela normal!';
-});
+//     return 'SUCESSO! A senha foi recriptografada e salva direto pelo Laravel na Railway. Agora remova esse código e tente logar na tela normal!';
+// });
 
 
 
