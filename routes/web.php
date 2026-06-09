afa038ddcb03a460d275d23f8f305d6a69d398fc
ViniciusVenturaM <vini.ventura2@gmail.com>
2026-06-09 10:33:14 -0300

    add rota de teste

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,17 @@ use App\Http\Controllers\ApiController;
 use App\Http\Controllers\LoginController;
 
 
+Route::get('/teste-autenticacao-secreta', function () {
+    $tentativa = \Illuminate\Support\Facades\Auth::attempt([
+        'usuario' => 'vinicius.ventura', 
+        'password' => 'coocafe2026'
+    ]);
+    
+    return $tentativa ? 'SENHA E USUÁRIO CORRETOS NO BANCO!' : 'FALHOU: O BANCO NÃO ACEITOU.';
+});
+
+
+
 Route::get('/login', [LoginController::class, 'mostrarLogin'])->name('login');
 Route::post('/login', [LoginController::class, 'autenticar'])->name('login.autenticar');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
